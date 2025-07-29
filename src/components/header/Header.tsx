898c4092cf956ff4bd560564237fc6195d63044f
Lucassml-boop <lucas.samuel23borges@gmail.com>
2025-07-29 10:33:33 -0300

    refactor: organizar componente Header em subpasta e atualizar estilos
    - Mover componente Header e CSS para subpasta 'header' para melhor organização
    - Adicionar fonte 'Work Sans' do Google Fonts no index.html e aplicar globalmente
    - Atualizar texto do título do header para "EXPLORE O UNIVERSO"
    - Corrigir caminho da importação do Header no App.tsx

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -4,7 +4,8 @@ export default function Header() {
   return (
     <header className="header">
       <img src="/src/assets/logo/marvel-logo.png" alt="Marvel Logo" className="header-logo" />
-      <span className="header-title">Search heros</span>
+      <span className="header-title"><h2>EXPLORE O UNIVERSO</h2></span>
+      
     </header>
   )
 }
