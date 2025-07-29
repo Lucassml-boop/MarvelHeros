cdc50349aaa7f135723c4a28854fc4080d0860e9
Lucassml-boop <lucas.samuel23borges@gmail.com>
2025-07-29 10:15:05 -0300

    feat: adiciona componente Header com estilo básico

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+import './Header.css'
+
+export default function Header() {
+  return (
+    <header className="header">
+      <img src="/src/assets/logo/marvel-logo.png" alt="Marvel Logo" className="header-logo" />
+      <span className="header-title">Search heros</span>
+    </header>
+  )
+}
