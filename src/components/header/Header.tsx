ef89b3c24091efb91500529a0b5bf47867d48e7a
Lucassml-boop <lucas.samuel23borges@gmail.com>
2025-07-30 14:07:04 -0300

    ajuste do import para o deploy

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -3,7 +3,7 @@ import './Header.css'
 export default function Header() {
   return (
     <header className="header">
-      <img src="src\assets\logo\Group@1,5x.svg" alt="Marvel Logo" className="header-logo" />
+      <img src="\assets\logo\Group@1,5x.svg" alt="Marvel Logo" className="header-logo" />
       <h2 className="header-title">EXPLORE O UNIVERSO</h2>
       <h4 className="header-sub-title">
         Mergulhe no domínio deslumbrante de todos os personagens clássicos que você ama - e aqueles que você descobrirá em breve!
